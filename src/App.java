5b1276b1faa975f32b098e016b267dd5a32ba44d
erikaMurcia18 <erikamurciagonzalez3@gmail.com>
2026-08-21 07:37:08 -0600

    subirtrabajito

---
+++ src/App.java
@@ -0,0 +1,111 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        /*
+        int a= 10, b= 3;
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+        
+// && = and, || es = our, ! = negacion//
+
+        boolean mayorDeEdad = true;
+        boolean tieneCarnet = false;
+
+        System.out.println(mayorDeEdad && tieneCarnet);
+        System.out.println(mayorDeEdad || tieneCarnet);
+        System.out.println(!mayorDeEdad);
+        
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("¿como te llamas?");
+        String nombre = sc.nextLine();
+
+        System.out.print("¿cuantos años tienes?");
+        int edad = sc.nextInt();
+
+        //System.out.println("hola," +nombre+ ". Tienes" +edad+ "años.");//
+
+        sc.close();
+        
+        
+        
+         
+
+        System.out.println("dame la base");
+        double base = sc.nextDouble();   
+
+        System.out.println("dame la altura");
+        double altura = sc.nextDouble();
+
+        //System.out.println("tu area es"+ area);
+
+         sc.close();
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Nota 1:");
+        double nota1 = sc.nextDouble();
+
+          
+        System.out.print("Nota 2:");
+        double nota2 = sc.nextDouble();
+
+          
+        System.out.print("Nota 3:");
+        double nota3 = sc.nextDouble();
+
+        double promedio =(nota1+nota2+nota3)/3;
+
+        sc.close();
+
+        Scanner sc = new Scanner(System.in);
+
+        Systemout.print(nota:);
+         int nota = sc.nextDouble();
+
+        
+
+        
+
+
+        
+*/
+
+
+
+System.out.println("Ingresa un número: ");
+        int nota = sc.nextInt();
+
+        if (nota >= 90){
+            System.out.println("Excelente");
+        } else if (nota >= 70){
+             System.out.println("Bueno");
+        } else if (nota >= 60){
+            System.out.println("Aprobado");
+        }else{
+             System.out.println("Reprobado");
+
+        }
+
+
+          
+
+
+
+
+
+
+        
+
+
+
+
+
+
+    }
+}
