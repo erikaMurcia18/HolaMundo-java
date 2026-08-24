36d3ffc717af4e29f7833e709348141b8a370419
erikaMurcia18 <erikamurciagonzalez3@gmail.com>
2026-08-23 20:53:26 -0500

    miclase.java

---
+++ src/MiClase.java
@@ -0,0 +1,15 @@
+public class MiClase {
+    private int edad = 22;
+    private double altura = 1,;
+    private char sexo = 'F';
+    private boolean aprobado = false;
+    private String nombre = "Erika";
+    private String saludo ="Hola" + nombre;
+    private double nota;
+            
+    
+
+
+}
+
+
