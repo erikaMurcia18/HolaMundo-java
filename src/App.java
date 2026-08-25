de414520e434b0c4ca07c3fe81fc8a4103997fda
erikaMurcia18 <erikamurciagonzalez3@gmail.com>
2026-08-24 20:42:05 -0500

    tarea poo

--- src/App.java
+++ src/App.java
@@ -78,7 +78,7 @@ public class App {
 
 
 
-System.out.println("Ingresa un número: ");
+/*System.out.println("Ingresa un número: ");
         int nota = sc.nextInt();
 
         if (nota >= 90){
@@ -90,22 +90,113 @@ System.out.println("Ingresa un número: ");
         }else{
              System.out.println("Reprobado");
 
+        }*/
+
+
+             // Tarea area de un rectangulo//
+    /*System .out.println("ingresa valor en numero del largo de la figura:");
+    double largo = sc.nextDouble();
+
+    System .out.println("ingresa valor en numero del ancho de la figura:");
+    double ancho = sc.nextDouble();
+
+    double area = largo *ancho;
+
+    System.out.print("El area del rectangulo es:  " + area);
+
+         sc.close();
+
+
+      }
+}*/
+//tarea area de un cuadrado//
+/*System.out.println("Ingresa valor en numero, del primer lado del cuadrado: ");
+        double lado1 = sc.nextDouble();
+    
+System.out.println("Ingresa valor en numero, del segundo lado del cuadrado: ");
+        double lado2 = sc.nextDouble();
+
+double area = lado1 * lado2;
+
+System.out.print("El area del cuadrado es: " + area);
+
+        sc.close();
+    }
+}*/
+// area de un circulo//
+/*System.out.println("Ingresa valor en numero del radio del circulo: ");
+        double radio = sc.nextDouble();
+double pi=3.1416;
+double area = pi * radio * radio;
+
+System.out.print("El area del circulo es: " + area);
+
+        sc.close();
+    }
+}*/
+//promedio//
+
+/*System.out.println("Ingresa primer valor en numero: ");
+        int valor1 = sc.nextInt();
+
+System.out.println("Ingresa segundo valor en numero: ");
+        int valor2 = sc.nextInt();
+
+System.out.println("Ingresa tercer valor en numero: ");
+        int valor3 = sc.nextInt();
+
+int promedio = (valor1 + valor2 + valor3) / 3;
+
+System.out.print("El promedio es: " + promedio);
+
+        sc.close();
+    }
+}*/
+
+// nota y promedio//
+
+/*System.out.println("Ingresa la nota1: ");
+        int nota1 = sc.nextInt();
+
+System.out.println("Ingresa la nota2: ");
+        int nota2 = sc.nextInt();
+
+System.out.println("Ingresa la nota3: ");
+        int nota3 = sc.nextInt();
+
+int promedio = (nota1 + nota2 + nota3) / 3;
+
+System.out.print(promedio);
+
+if (promedio >= 90){
+            System.out.println("Excelente");
+        } else if (promedio >= 70){
+             System.out.println("Bueno");
+        } else if (promedio >= 60){
+            System.out.println("Aprobado");
+        }else{
+             System.out.println("Reprobado");
+
         }
 
 
-          
 
 
+        sc.close();
+    }
+}*/
 
+// mayor de edad//
 
+System.out.println("Ingresa tu edad: ");
+        int edad = sc.nextInt();
 
+if (edad >= 18){
+            System.out.println("Eres mayor de edad");
+        } else {
+             System.out.println("Eres menor de edad");
+        }
 
-        
-
-
-
-
-
-
+        sc.close();
     }
 }
